32e62b909847344e41b205c0e789700b595b9964
Faktor 10 <oreillyross@gmail.com>
2021-01-15 15:20:36 +0000

    added better solution

--- maximum-sub-array.js
+++ maximum-sub-array.js
@@ -1,8 +1,8 @@
 // Naieve Brut force approach
-const arr = [1,4,6,2,3]
+const arr = [1,44,6,2,4,6,2,4,24,62,4,62,46,24,6,2,4,6,2,4,6,2]
 const k = 2
 
-function max_sub(k, arr) {
+function max_sub_brute(k, arr) {
   let maxSum = 0;
   let windowSum = 0;
 
@@ -15,5 +15,28 @@ function max_sub(k, arr) {
   }
   return maxSum
 }
+//===================================
+//Better solution with O(n) time and O(1) space
 
+function max_sub(k, arr) {
+  let maxSum = 0;
+  let windowSum = 0;
+  let windowStart = 0;
+
+  for(let window_end = 0; window_end < arr.length; window_end++) {
+    windowSum += arr[window_end];
+    // slide window needs to stretch out first
+    if (window_end >= k) {
+       windowSum -= arr[windowStart]
+       maxSum = Math.max(windowSum, maxSum)
+       windowStart += 1
+    }
+  }
+  return maxSum;
+}
+
+
+
+
+console.log(max_sub_brute(k, arr))
 console.log(max_sub(k, arr))
