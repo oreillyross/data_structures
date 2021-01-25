ae543b8eac475353dede77118c49056840573962
Faktor 10 <oreillyross@gmail.com>
2021-01-25 12:31:58 +0000

    add unsorted problem

--- remove-dups.js
+++ remove-dups.js
@@ -13,3 +13,23 @@ return nextNonDuplicate;
 }
 
 console.log(remove_dups([1,1,2,2,3,5,6,6,8,34]))
+
+/*
+  A similar problem is one where you are asked to remove duplicate elements based on a given key from an unsorted array.
+  Again the Big-O will be O(n) linear time and O(1) constant in space
+*/
+
+const remove_dup = function(arr, key) {
+let nextElement = 0;
+
+arr.forEach((el, idx, array) => {
+  if (el != key) {
+    array[nextElement] = el;
+    nextElement += 1;
+  }
+})
+
+return nextElement;
+}
+
+console.log(remove_dup([1,1,5,3,2], 1))
