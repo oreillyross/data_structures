cb951dc8a09a5d4894862db659b4c0653eb1f806
Faktor 10 <oreillyross@gmail.com>
2021-02-16 17:45:44 +0000

    patter_sliding

---
+++ find-word-concatenation.js
@@ -0,0 +1,46 @@
+function find_word_concat(str, words) {
+  if (words.length === 0 || words[0].length === 0) {
+    return []
+  }
+
+  let wordFrequency = {};
+
+  words.forEach(word => {
+    if (!(word in wordFrequency)) {
+      wordFrequency[word] = 0;
+    }
+    wordFrequency[word] += 1
+  }) 
+
+  const resultIndices = [],
+        wordsCount = words.length;
+        wordLength = word[0].length;
+
+  for (let i = 0; i < (str.length - wordsCount * wordLength) + 1; i++) {
+    const wordSeen = {};
+    for (let j = 0; j < wordsCount; j++) {
+      next_word_index = i + j * wordLength;
+    word = str.substring(next_word_index, next_word_index + wordLength);
+    if (!(word in wordFrequency)) {
+      break;
+    }
+
+    if (!(word in wordSeen)) {
+      wordSeen[word] = 0
+    }
+    wordSeen[word] += 1;
+   }
+
+   if (wordSeen[word] > (wordFrequency[word] || 0)) {
+     break;
+   }
+    
+   if (j + 1 === wordsCount) {
+     resultIndices.push(i) 
+   } 
+  }      
+  return resultIndices;
+}
+
+console.log(find_word_concat('catfoxcat', ['cat', 'fox']));
+
