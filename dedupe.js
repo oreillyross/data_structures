6a993e0ea6f804230c80dbf5b2930b12795e6ae8
Ross O'Reilly <oreillyross@gmail.com>
2021-06-07 21:53:19 +0200

    dedupe

--- dedupe.js
+++ dedupe.js
@@ -1,3 +1,6 @@
+// this is pre-es6 way to iterate
+// essential is the object to store what element has come before
+// and also the array to store non-duplicate elements
 function dedupe(str) {
   let dupChars = {}
   let newStr = []
@@ -9,5 +12,10 @@ function dedupe(str) {
     }
     return newStr.join("")
 }
+//this uses the Set to replace the need for sepearate object and array
+function dedupeEs6(str) {
+  const newStr = new Set(str)
+  return Array.from(newStr).join("")
+}
 
-module.exports = {dedupe}
+module.exports = {dedupe, dedupeEs6}
--- dedupe.test.js
+++ dedupe.test.js
@@ -1,7 +1,11 @@
-const {dedupe} = require("./dedupe")
+const {dedupe, dedupeEs6} = require("./dedupe")
 
 describe("dedupe", () => {
     it("should remove any duplicate chars", () => {
         expect(dedupe("aaabscbdddfdd")).toEqual("abscdf")
+    })
+
+    it("should remove any duplicate chars using the new Set function", () => {
+        expect(dedupeEs6("aaabscbdddfdd")).toEqual("abscdf")
     })
 })
